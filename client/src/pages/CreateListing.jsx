a0df68b6825a94108ff0e89d134687fe0f052208
ShakilTalukder07 <shakil.talukder07@gmail.com>
2024-06-23 23:24:42 +0600

    taka add

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -499,7 +499,7 @@ const CreateListing = () => {
                 required
               />
               <p>Now, set your PRICE</p>
-              <span>$</span>
+              <span>৳</span>
               <input
                 type="number"
                 placeholder="100"
